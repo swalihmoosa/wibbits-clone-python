f1634f54d0b1c5ecb040b7365f700ed8cb40c1f3
swalihmoosa <swalihmoosa72@gmail.com>
2021-11-17 16:04:03 +0530

    Class-FeatureAdmin added

--- web/admin.py
+++ web/admin.py
@@ -5,4 +5,8 @@ from web.models import Customers, Feature, Subscribe
 
 admin.site.register(Subscribe)
 admin.site.register(Customers)
-admin.site.register(Feature)
+
+class FeatureAdmin(admin.ModelAdmin):
+    list_display = ["id", "title","testimonial_author", "author_designation" ]
+
+admin.site.register(Feature, FeatureAdmin)

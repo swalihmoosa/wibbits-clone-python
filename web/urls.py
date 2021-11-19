24e537d40b1b040224e581daa91dfb71226f5a4b
swalihmoosa <swalihmoosa72@gmail.com>
2021-11-19 12:11:21 +0530

    manual validation in footer-section ongoing

--- web/urls.py
+++ web/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from django.urls.conf import include
 
-from web.views import index, subscribe
+from web.views import index, subscribe, contact
 
 app_name = "web"
 
 
 urlpatterns = [
     path('',index,name="index"),
-    path('subscribe',subscribe,name="subscribe")
+    path('subscribe',subscribe,name="subscribe"),
+    path('contact',contact,name="contact")
+
 ]
--- web/views.py
+++ web/views.py
@@ -2,7 +2,7 @@ import json
 from django.http.response import HttpResponse
 from django.shortcuts import render, redirect
 
-from web.models import Blog, Feature, Marketing, Product, Review, Subscribe, Customers, Testimonial
+from web.models import Blog, Contact, Feature, Marketing, Product, Review, Subscribe, Customers, Testimonial
 
 
 def index(request):
@@ -32,9 +32,55 @@ def index(request):
 
 def subscribe(request):
     email = request.POST.get("email")
+
     if not Subscribe.objects.filter(email=email).exists() and email:
         Subscribe.objects.create(
             email = email
+        )
+
+        response_data = {
+            "status" : "success",
+            "title" : "Successfully Registered",
+            "message" : "You are Subscribed to the News Letter"
+        }
+    elif not email:
+        response_data = {
+            "status" : "error",
+            "title" : "Enter a Valid Email",
+            "message" : "You Enter a Invalid Email,Check the Email"
+        }
+    else:
+        response_data = {
+            "status" : "error",
+            "title" : "Already Registered",
+            "message" : "You are Already Subscribed to the News Letter,no need to Subscribe again"
+        }
+
+    return HttpResponse(json.dumps(response_data),content_type="application/javascript")
+
+
+def contact(request):
+    email = request.POST.get("email")
+    first_name = request.POST.get("first_name")
+    last_name = request.POST.get("last_name")
+    company = request.POST.get("company")
+    company_size = request.POST.get("company_size")
+    industry = request.POST.get("industry")
+    jobe_role = request.POST.get("jobe_role")
+    country = request.POST.get("country")
+    user_agreement = request.POST.get("user_agreement")
+    
+    if not Contact.objects.filter(email=email).exists() and email:
+        Contact.objects.create(
+            email = email,
+            first_name = first_name,
+            last_name = last_name,
+            company = company,
+            company_size = company_size,
+            industry = industry,
+            jobe_role = jobe_role,
+            country = country,
+            user_agreement = user_agreement
         )
 
         response_data = {

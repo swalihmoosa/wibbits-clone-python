47620729b4229200dd0e99763798821544565ca8
swalihmoosa <swalihmoosa72@gmail.com>
2021-11-19 17:00:12 +0530

    forms.py ongoing

--- web/forms.py
+++ web/forms.py
@@ -1,5 +1,6 @@
 from django.db.models import fields
-from django.forms import ModelForm, models
+from django.forms import ModelForm
+from django.forms.widgets import EmailInput, TextInput
 from web.models import Contact
 
 
@@ -7,3 +8,11 @@ class ContactForm(ModelForm):
     class Meta:
         model = Contact
         fields = "__all__"
+        widgets = {
+            "email" : EmailInput(attrs={"placeholder" : "Enter Your Email"}),
+            "first_name" : TextInput(attrs={"placeholder" : "Enter Your First Name"}),
+            "last_name" : TextInput(attrs={"placeholder" : "Enter Your Last Name"}),
+            "company" : TextInput(attrs={"placeholder" : "Enter Your Company"}),
+            # "company" : EmailInput(attrs={"placeholder" : "Enter Your Company"})
+            # "email" : EmailInput(attrs={"placeholder" : "Enter Your Email"})
+        }

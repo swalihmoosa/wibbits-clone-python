a16e360ca60c18c0cfe57a7acff99915459d8458
swalihmoosa <swalihmoosa72@gmail.com>
2021-11-20 10:46:04 +0530

    Choice field customised in footer-section

--- web/forms.py
+++ web/forms.py
@@ -1,10 +1,22 @@
+from django import forms
 from django.db.models import fields
 from django.forms import ModelForm
-from django.forms.widgets import EmailInput, TextInput
-from web.models import Contact
+from django.forms.widgets import EmailInput, Select, TextInput
+from web.models import COMPANY_SIZE, COUNTRY, INDUSTRY, JOB_ROLE, Contact
+
+
+EMPTY_COMPANY_SIZE = (("", "Comapany Size"),) + COMPANY_SIZE
+EMPTY_INDUSTRY = (("", "Industry"),) + INDUSTRY
+EMPTY_JOB_ROLE = (("", "Job Role"),) + JOB_ROLE
+EMPTY_COUNTRY = (("", "Country"),) + COUNTRY
 
 
 class ContactForm(ModelForm):
+    company_size = forms.ChoiceField(choices=EMPTY_COMPANY_SIZE)
+    industry = forms.ChoiceField(choices=EMPTY_INDUSTRY)
+    jobe_role = forms.ChoiceField(choices=EMPTY_JOB_ROLE)
+    country = forms.ChoiceField(choices=EMPTY_COUNTRY)
+
     class Meta:
         model = Contact
         fields = "__all__"
@@ -13,6 +25,8 @@ class ContactForm(ModelForm):
             "first_name" : TextInput(attrs={"placeholder" : "Enter Your First Name"}),
             "last_name" : TextInput(attrs={"placeholder" : "Enter Your Last Name"}),
             "company" : TextInput(attrs={"placeholder" : "Enter Your Company"}),
-            # "company" : EmailInput(attrs={"placeholder" : "Enter Your Company"})
-            # "email" : EmailInput(attrs={"placeholder" : "Enter Your Email"})
+            "company_size" : Select(),
+            "industry" : Select(),
+            "jobe_role" : Select(),
+            "country" : Select()
         }
